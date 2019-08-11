f6b5859d4bacd62457d364c0757cfc1d87fd701e
Fredrik Fossum <fredrik.fossum@gmail.com>
2019-08-11 21:31:07 +0200

    connect to redis

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
+import Control.Concurrent (threadDelay)
+import Control.Concurrent.Async (race_)
+import Control.Exception (SomeException, catch)
+import Control.Monad (forever)
 import CryptoService (mkFakeCryptoService)
+import qualified Database.Redis as Redis
 import Lib
 import Network.Wai.Handler.Warp
 import Server (app)
@@ -12,9 +19,27 @@ port = 8080
 
 main :: IO ()
 main = do
+  redisConnection <-
+    Redis.checkedConnect
+      Redis.defaultConnectInfo
+        { Redis.connectHost = "172.17.0.2"
+        } <*
+      putStrLn "Connected to redis"
+  pubSubController <- Redis.newPubSubController [] []
   let cryptoSvc = mkFakeCryptoService
   userSvc <- mkInMemoryUserService cryptoSvc
   wsApp <- mkWebsocketApp userSvc
   putStrLn $ "warp " <> warpVersion
   putStrLn $ "Starting server listening on port " <> (show port)
-  run port (app userSvc wsApp)
+  race_
+    (run port (app userSvc wsApp))
+    ( forever $
+      Redis.pubSubForever
+        redisConnection
+        pubSubController
+        (putStrLn "Redis pubsub active") `catch`
+      ( \(err :: SomeException) ->
+        putStrLn ("Redis pubsub error: " <> show err) *>
+          threadDelay 10000
+      )
+    )
